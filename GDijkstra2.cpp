7e709c4337c902a18707c75429ded8cb7d06903e
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2024-11-06 11:54:10 -0500

    Update GDijkstra2.cpp

--- GDijkstra2.cpp
+++ GDijkstra2.cpp
@@ -1,3 +1,9 @@
+/*******************************************
+Fecha: 1 octubre 2024
+Autor: Santiago Camargo
+Tema: Taller Arboles
+Estructuras de datos
+******************************************/
 #include <iostream>
 #include <vector>
 #include <stack>
